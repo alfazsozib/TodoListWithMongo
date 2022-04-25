f5aa98f1802e60ff120a02f1293855f0f3778170
alfazsozib <alfazsozib@gmail.com>
2022-04-26 02:26:15 +0600

    3rd changes

--- app.js
+++ app.js
@@ -95,6 +95,13 @@ app.post('/delete',(req,res)=>{
 
 
 // SERVER RUNNING
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server started on port : 3000");
+
+let port = process.env.PORT;
+if (port==null || port==""){
+  port = 3000;
+}
+
+app.listen(port,()=>{
+  console.log("server has started!")
 });
+
